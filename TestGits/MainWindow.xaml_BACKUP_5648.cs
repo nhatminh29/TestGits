32e067e9a031a6e38f6a6fc35aeb8da48a5bdfd1
quanghai32 <quanghai.dt4@gmail.com>
2017-12-21 18:33:48 +0700

    change in quynh

---
+++ TestGits/MainWindow.xaml_BACKUP_5648.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestGits
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+<<<<<<< HEAD
+            //1
+=======
+            //change src
+            //change des
+            //change src 2
+            //change des 2
+>>>>>>> b63ca7584166257463dfa2a521a14713f688545b
+        }
+    }
+}
